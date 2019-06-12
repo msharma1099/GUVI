3960a1f89ac0af3438e3f6084c81494509bcf43f
msharma1099 <39601443+msharma1099@users.noreply.github.com>
2019-06-12 08:08:57 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    char a = sc.next().charAt(0);
+	    if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U'){
+	        System.out.println("VOWEL");
+	    }
+	    else{
+		    System.out.println("CONSTANT");
+	    }
+	}
+}
